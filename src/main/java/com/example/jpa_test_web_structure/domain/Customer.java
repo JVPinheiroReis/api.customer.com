cd49a437f6ec89695fcc40975d88347bce2dc7b8
João Victor <joaovictorpinheiro1510@gmail.com>
2026-01-17 15:19:26 -0300

    feat(CustomerRespository): add repository

--- src/main/java/com/example/jpa_test_web_structure/domain/Customer.java
+++ src/main/java/com/example/jpa_test_web_structure/domain/Customer.java
@@ -32,15 +32,19 @@ public class Customer {
         );
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/com/example/jpa_test_web_structure/respository/CustomerRepository.java
+++ src/main/java/com/example/jpa_test_web_structure/respository/CustomerRepository.java
@@ -3,9 +3,15 @@ package com.example.jpa_test_web_structure.respository;
 import com.example.jpa_test_web_structure.domain.Customer;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository
+    extends
+        PagingAndSortingRepository<Customer, Long>,
+        CrudRepository<Customer, Long>
+{
     Customer findById(long id);
-    List<Customer> findByFirstName(String firstName);
-    List<Customer> findByLastName(String lastName);
+    List<Customer> findByFirstName(@Param("name") String firstName);
+    List<Customer> findByLastName(@Param("name") String lastName);
 }
